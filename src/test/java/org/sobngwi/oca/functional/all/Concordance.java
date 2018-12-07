7088288f678cc46b34d80f66b5148dafb6dea592
sobngwi@gmail.com <sobngwi@gmail.com>
2018-12-07 19:15:02 +0100

    generic version of Exception Wrapper mecanism.

--- src/test/java/org/sobngwi/oca/functional/all/Concordance.java
+++ src/test/java/org/sobngwi/oca/functional/all/Concordance.java
@@ -45,39 +45,55 @@ public class Concordance {
     public void concordanceWithExceptionHandling() throws IOException {
 
         String relativePath = "/Users/sobngwi/intelliJ/codebase/certification/oca/src/";
-        List<String> fileNames = Arrays.asList(relativePath +  "Bad Book.txt"  , relativePath + "PrideAndPrejudice.txt",  relativePath + "ACopy.txt");
+        List<String> fileNames = Arrays.asList(relativePath + "Bad Book.txt", relativePath + "PrideAndPrejudice.txt", relativePath + "ACopy.txt");
 
-            fileNames
-                    .stream()
+        fileNames
+                .stream()
 
-                    //.map(fileName -> Paths.get(fileName))
-                    //.flatMap(Files::lines)
-                    .map(Paths::get)
-                    .map(Concordance::lines)
-                    .peek( o -> { if ( ! o.isPresent()) System.err.println("Bad file !"); })
-                    .filter(Optional::isPresent)
-                    .flatMap(Optional::get)
-                    .flatMap(WORD_BREAK::splitAsStream)
-                    .filter(isEmpty.negate())
-                    .map(String::toLowerCase)
-                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                    .entrySet().stream()
-                    .sorted(byValueOrderReverse)
-                    .limit(10)
-                    .map(l -> String.format("%20s  :  %5d", l.getKey(), l.getValue()))
-                    .forEach(System.out::println);
+                //.map(fileName -> Paths.get(fileName))
+                //.flatMap(Files::lines)
+                .map(Paths::get)
+                //.map(Concordance::lines)
+                .map(wrapException( p -> Files.lines(p)))
+                .peek(o -> {
+                    if (!o.isPresent()) System.err.println("Bad file !");
+                })
+                .filter(Optional::isPresent)
+                .flatMap(Optional::get)
+                .flatMap(WORD_BREAK::splitAsStream)
+                .filter(isEmpty.negate())
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(byValueOrderReverse)
+                .limit(10)
+                .map(l -> String.format("%20s  :  %5d", l.getKey(), l.getValue()))
+                .forEach(System.out::println);
     }
 
-    private static Optional<Stream<String>> lines (Path p){
-        try{
-            return Optional.of(Files.lines(p)) ;
-        }
-        catch (IOException ioe){
-          // Sol 1  throw new RuntimeException( ioe) ;
+    private static Optional<Stream<String>> lines(Path p) {
+        try {
+            return Optional.of(Files.lines(p));
+        } catch (IOException ioe) {
+            // Sol 1  throw new RuntimeException( ioe) ;
             System.err.println(ioe);
             return Optional.empty(); // sol 2 ignore the exception. ie the files.
         }
     }
 
+    private interface exceptionFunction<E, F> {
+        F apply(E e) throws Exception;
+    }
 
+    private static <E, F> Function<E, Optional<F>> wrapException(exceptionFunction<E, F> op) {
+
+        return e -> {
+            try {
+                return Optional.of(op.apply(e));
+            } catch (Throwable t) {
+                System.err.println(t.getMessage());
+                return Optional.empty();
+            }
+        };
+    }
 }
