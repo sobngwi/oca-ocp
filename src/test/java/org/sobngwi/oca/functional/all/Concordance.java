60f3f1322aacad545c23520c983ba6afc6ab58e6
sobngwi@gmail.com <sobngwi@gmail.com>
2018-12-07 18:53:30 +0100

    Version 2 of Concordance with Optional and cath exception , return Empty Stream.

--- src/test/java/org/sobngwi/oca/functional/all/Concordance.java
+++ src/test/java/org/sobngwi/oca/functional/all/Concordance.java
@@ -1,16 +1,18 @@
 package org.sobngwi.oca.functional.all;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Concordance {
 
@@ -21,20 +23,61 @@ public class Concordance {
 
 
     @Test
+    @Ignore
     public void concordance() throws IOException {
 
-                Files
-                        .lines(Paths.get("/Users/sobngwi/intelliJ/codebase/certification/oca/src/PrideAndPrejudice.txt"))
-                        .flatMap(WORD_BREAK::splitAsStream)
-                        .filter(isEmpty.negate())
-                        .map(String::toLowerCase)
-                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                        .entrySet().stream()
-                        .sorted(byValueOrderReverse)
-                        .limit(10)
-                        .map(l -> String.format("%20s  :  %5d", l.getKey(), l.getValue()))
-                        .forEach(System.out::println);
+        Files
+                .lines(Paths.get("/Users/sobngwi/intelliJ/codebase/certification/oca/src/PrideAndPrejudice.txt"))
+                .flatMap(WORD_BREAK::splitAsStream)
+                .filter(isEmpty.negate())
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(byValueOrderReverse)
+                .limit(10)
+                .map(l -> String.format("%20s  :  %5d", l.getKey(), l.getValue()))
+                .forEach(System.out::println);
 
 
     }
+
+    @Test
+    public void concordanceWithExceptionHandling() throws IOException {
+
+        String relativePath = "/Users/sobngwi/intelliJ/codebase/certification/oca/src/";
+        List<String> fileNames = Arrays.asList(relativePath +  "Bad Book.txt"  , relativePath + "PrideAndPrejudice.txt",  relativePath + "ACopy.txt");
+
+            fileNames
+                    .stream()
+
+                    //.map(fileName -> Paths.get(fileName))
+                    //.flatMap(Files::lines)
+                    .map(Paths::get)
+                    .map(Concordance::lines)
+                    .peek( o -> { if ( ! o.isPresent()) System.err.println("Bad file !"); })
+                    .filter(Optional::isPresent)
+                    .flatMap(Optional::get)
+                    .flatMap(WORD_BREAK::splitAsStream)
+                    .filter(isEmpty.negate())
+                    .map(String::toLowerCase)
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                    .entrySet().stream()
+                    .sorted(byValueOrderReverse)
+                    .limit(10)
+                    .map(l -> String.format("%20s  :  %5d", l.getKey(), l.getValue()))
+                    .forEach(System.out::println);
+    }
+
+    private static Optional<Stream<String>> lines (Path p){
+        try{
+            return Optional.of(Files.lines(p)) ;
+        }
+        catch (IOException ioe){
+          // Sol 1  throw new RuntimeException( ioe) ;
+            System.err.println(ioe);
+            return Optional.empty(); // sol 2 ignore the exception. ie the files.
+        }
+    }
+
+
 }
